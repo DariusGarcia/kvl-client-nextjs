98e997f0b0eddf5a98e1c96df4f0bd5566c2b6b3
Darius Garcia <dariusgarcia888@gmail.com>
2023-06-19 21:48:19 -0700

    remove meta

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,13 @@ import Head from 'next/head'
 import NavBar from '@/components/navbar'
 import Footer from '@/components/footer'
 
-// import Footer from '@/components/footer'
-
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>KVL Communications</title>
       </Head>
-
       <NavBar />
-
       <Component {...pageProps} />
       <Footer />
     </>
