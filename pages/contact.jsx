6af3ce5c0947a36f4f5e4a0684d02cdd8012d74a
Darius Garcia <dariusgarcia888@gmail.com>
2023-11-14 12:36:14 -0800

    styling

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -6,9 +6,9 @@ import {
 
 export default function ContactPage() {
   return (
-    <div className='relative isolate bg-white my-24'>
+    <div className='relative isolate bg-white'>
       <div className='mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2'>
-        <div className='relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48'>
+        <div className='relative px-6 pb-20 pt-12 sm:pt-32 lg:static lg:px-8 lg:py-48'>
           <div className='mx-auto max-w-xl lg:mx-0 lg:max-w-lg'>
             <div className='absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2'>
               <svg
