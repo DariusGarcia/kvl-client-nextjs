2043c1f716d834029d633d13daecea551fc0650a
Darius Garcia <dariusgarcia888@gmail.com>
2023-11-15 13:55:28 -0800

    add error and success messages

--- components/mailchimpForm.jsx
+++ components/mailchimpForm.jsx
@@ -1,18 +1,33 @@
 import { useState } from 'react'
 import axios from 'axios'
+import { XCircleIcon } from '@heroicons/react/20/solid'
+import { CheckCircleIcon, XMarkIcon } from '@heroicons/react/20/solid'
 
 export default function MailChimpForm() {
   const [email, setEmail] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
   const handleSubscribe = async (e) => {
     e.preventDefault()
     try {
       const response = await axios.post('/api/subscribe', { email })
       console.log('Subscription successful:', response.data)
+      setSuccessMessage('Subscription successful! Thank you for subscribing.')
+      setErrorMessage('') // Reset error message if there was one before
+      setEmail('')
     } catch (error) {
       console.error('Subscription error:', error)
+      setSuccessMessage('') // Reset success message if there was one before
+      setErrorMessage('Subscription failed. Please try again.') // Set error message
     }
   }
+
+  const handleDismiss = () => {
+    setSuccessMessage('')
+    setErrorMessage('')
+  }
+
   return (
     <div className='bg-gray-50 w-full max-w-4xl flex flex-col justify-center items-center shadow sm:rounded-lg'>
       <div className='px-4 py-12 sm:p-12'>
@@ -43,6 +58,56 @@ export default function MailChimpForm() {
             Subscribe
           </button>
         </form>
+        {successMessage && (
+          <div className='rounded-md bg-green-50 p-4 mt-6'>
+            <div className='flex'>
+              <div className='flex-shrink-0'>
+                <CheckCircleIcon
+                  className='h-5 w-5 text-green-400'
+                  aria-hidden='true'
+                />
+              </div>
+              <div className='ml-3'>
+                <p className='text-sm font-medium text-green-800'>
+                  {successMessage}
+                </p>
+              </div>
+              <div className='ml-auto pl-3'>
+                <div className='-mx-1.5 -my-1.5'>
+                  <button
+                    type='button'
+                    onClick={handleDismiss}
+                    className='inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50'>
+                    <span className='sr-only'>Dismiss</span>
+                    <XMarkIcon className='h-5 w-5' aria-hidden='true' />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        {errorMessage && (
+          <div className='rounded-md bg-red-50 p-4 mt-6'>
+            <div className='flex'>
+              <div className='flex-shrink-0'>
+                <XCircleIcon
+                  className='h-5 w-5 text-red-400'
+                  aria-hidden='true'
+                />
+              </div>
+              <div className='ml-3'>
+                <h3 className='text-sm font-medium text-red-800'>
+                  {errorMessage}
+                </h3>
+                <div className='mt-2 text-sm text-red-700'>
+                  <ul role='list' className='list-disc space-y-1 pl-5'>
+                    <li>Email might already be subscribed.</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
