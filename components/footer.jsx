b1886d5247a3400442311c79c77647f72885eca0
Darius Garcia <dariusgarcia888@gmail.com>
2023-11-16 15:02:25 -0800

    styling - footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -95,7 +95,7 @@ export default function Footer() {
                   onClick={handleSubscribe}
                   className='flex w-full items-center justify-center rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500'>
                   {loading ? (
-                    <div className='w-full flex items-center justify-center'>
+                    <div className='w-14 flex items-center justify-center'>
                       <RingLoader
                         color={'#ffffff'}
                         loading={loading}
